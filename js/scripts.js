8c93d323af0c95a46921f755e02695fe8a87493b
dcouch440 <dcouch440@gmail.com>
2021-01-13 16:26:14 -0800

    fix syntax in scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,20 @@
 const add = function(number1, number2) {
   return number1 + number2;
 };
-
 const subtract = function(number1, number2) {
   return number1 - number2;
 };
-
 const multiply = function(number1, number2) {
   return number1 * number2;
 };
-
 const divide = function(number1, number2) {
   return number1 / number2;
 };
 
 $(document).ready(function() {
-  const N1 = parseInt($("#val1").val());
-  const N2 = parseInt($("#val2").val());
-
-  $("button#add").click(function(event, number1 = N1, number2 = N2) {
+  $("button#add").click(function(event) {
+    const number1 = parseInt($("#val1").val());
+    const number2 = parseInt($("#val2").val());
     event.preventDefault()
     $("#output").text(add(number1, number2))
   });
@@ -28,7 +24,6 @@ $(document).ready(function() {
     const number2 = parseInt($("#val2").val());
     $("#output").text(subtract(number1, number2))
   });
-
   $("button#multipy").click(function(event) {
     event.preventDefault()
     const number1 = parseInt($("#val1").val());
@@ -41,5 +36,4 @@ $(document).ready(function() {
     const number2 = parseInt($("#val2").val());
     $("#output").text(divide(number1, number2))
   });
-  
 });
