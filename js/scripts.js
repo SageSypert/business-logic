a35f445d38758c73463243c989c3b73261343308
dcouch440 <dcouch440@gmail.com>
2021-01-13 16:24:53 -0800

    add function for add subtract divide and multiply

--- js/scripts.js
+++ js/scripts.js
@@ -15,11 +15,31 @@ const divide = function(number1, number2) {
 };
 
 $(document).ready(function() {
-  $("form#add").submit(function(event) {
-    event.preventDefault();
-    const number1 = parseInt($("#add1").val());
-    const number2 = parseInt($("#add2").val());
-    const result = add(number1, number2);
-    $("#output").text(result);
+  const N1 = parseInt($("#val1").val());
+  const N2 = parseInt($("#val2").val());
+
+  $("button#add").click(function(event, number1 = N1, number2 = N2) {
+    event.preventDefault()
+    $("#output").text(add(number1, number2))
   });
+  $("button#subtract").click(function(event) {
+    event.preventDefault()
+    const number1 = parseInt($("#val1").val());
+    const number2 = parseInt($("#val2").val());
+    $("#output").text(subtract(number1, number2))
+  });
+
+  $("button#multipy").click(function(event) {
+    event.preventDefault()
+    const number1 = parseInt($("#val1").val());
+    const number2 = parseInt($("#val2").val());
+    $("#output").text(multipy(number1, number2))
+  });
+  $("button#divide").click(function(event) {
+    event.preventDefault()
+    const number1 = parseInt($("#val1").val());
+    const number2 = parseInt($("#val2").val());
+    $("#output").text(divide(number1, number2))
+  });
+  
 });
